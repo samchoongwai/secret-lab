cf47731dab3ad3dcf02381007b60cadb501f5043
Sam Choong Wai <samchoongwai@gmail.com>
2021-08-15 07:48:53 +0000

    Fix CI CD scripts

--- .platform/hooks/postdeploy/50-laravel-initiation.sh
+++ .platform/hooks/postdeploy/50-laravel-initiation.sh
@@ -4,4 +4,7 @@ cp /var/app/current/.env.example /var/app/current/.env
 
 php /var/app/current/artisan key:generate
 
+php /var/app/current/artisan migrate
+
 chown -R webapp:webapp /var/app/current
+
